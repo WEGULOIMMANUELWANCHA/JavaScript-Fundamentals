54f0fe82e5b0cecb6a07ac3dd5def30522c107ed
todd-buck <btbuckjr@gmail.com>
2022-10-18 14:45:36 -0500

    Fixed issue with mole being moved after timer hits 0
    Fixed issue with multi-click moles.

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -10,16 +10,20 @@ function randomSquare() {
     square.forEach(className => {
         className.classList.remove('mole')
     })
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    randomPosition.classList.add('mole')
-    hitPosition = randomPosition.id
+    if(currentTime > 0) {
+        let randomPosition = square[Math.floor(Math.random() * 9)]
+        randomPosition.classList.add('mole')
+        hitPosition = randomPosition.id
+    }
 }
 
 square.forEach(id => {
     id.addEventListener('mouseup', () => {
         if(id.id === hitPosition) {
+            id.id = null
             result++
             score.textContent = result
+            id.classList.remove('mole')
         }
     })
 })
